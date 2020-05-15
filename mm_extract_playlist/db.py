0062d73d7d1d2508622e5ddc42c5b66b66e80166
laharah <laharah22@gmail.com>
2020-05-15 00:48:46 -0700

    start implemeting database calls

--- mm_extract_playlist/db.py
+++ mm_extract_playlist/db.py
@@ -1,8 +1,8 @@
 import sqlite3
 from . import utils
 
-def get_db(path=None):
-    pass
+def connect(path=None):
+    return sqlite3.connect(path)
 
 def get_all_playlists(db):
     "get a list of playlist objects with their songs"
