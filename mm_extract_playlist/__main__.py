a6c5077104aca14bed896d95150330a18cd8ac51
laharah <laharah22@gmail.com>
2020-05-15 22:59:12 -0700

    music folder replacement added

--- mm_extract_playlist/__main__.py
+++ mm_extract_playlist/__main__.py
@@ -1,11 +1,25 @@
+import itertools
+from pathlib import Path
+
 from . import database
 from . import m3u
+from . import track
 
 
-def main(db, output_dir):
+def main(db, output_dir, music_folder=None, overwrite=False):
     db = database.connect(db)
     playlists = database.get_all_playlists(db)
-    m3u.write_all(playlists, output_dir)
+    if music_folder:
+        music_folder = Path(music_folder)
+        orig_music_folder = track.common_base(
+            itertools.chain(*[p.tracks for p in playlists.values()]))
+        print(f"Calculated original music folder to be '{orig_music_folder}'")
+        replacement = (orig_music_folder, music_folder)
+        print(f"Re-routing to '{music_folder}'")
+    else:
+        replacement = None
+    m3u.write_all(playlists, output_dir, replace=replacement, overwrite=overwrite)
+    db.close()
 
 
 def entry_point():
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,22 @@
+import re
+import os
+from pathlib import Path
 from mm_extract_playlist import __main__ as m
+
 
 def test_main(tmp_path):
     m.main('tests/testdb.db', tmp_path)
     listing = list(tmp_path.iterdir())
-    print()
-    for p in listing:
-        print(p)
-        print(p.read_text())
     assert len(listing) == 2
 
+
+def test_main_redirect(tmp_path, capfd):
+    m.main('tests/testdb.db', tmp_path, music_folder='/pool/Media/Music')
+    listing = list(tmp_path.iterdir())
+    assert len(listing) == 2
+    out = capfd.readouterr().out
+    print(out)
+    assert re.search(r':\\Users\\testuser\\Desktop\\songs', out) is not None
+    p = tmp_path / 'KSP.m3u'
+    first = Path(p.read_text().splitlines()[0])
+    assert first.parts == (os.path.sep, 'pool', 'Media', 'Music', '02 Gipsy Danger.mp3')
